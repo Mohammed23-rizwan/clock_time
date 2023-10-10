8cc87aed19b20310a7d9c4a858c22b98e5ad7f58
Mohammed23-rizwan <63270257+Mohammed23-rizwan@users.noreply.github.com>
2023-10-10 12:39:10 +0530

    Update script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,6 +2,7 @@ function timing (){
     let get_hours = new Date().getHours();
     let get_minutes = new Date().getMinutes();
     let get_seconds = new Date().getSeconds();
+
     
     const hours = document.querySelector('#hours')
     hours.innerHTML = get_hours<10?"0"+get_hours:get_hours
