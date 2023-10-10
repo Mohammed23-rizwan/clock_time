cb23ed3fcff59a2bfddbfb8a9bb49dd267e2314d
Mohammed23-rizwan <rizuart23@gmail.com>
2023-10-10 12:36:40 +0530

    finished

---
+++ Assets/js/script.js
@@ -0,0 +1,14 @@
+function timing (){
+    let get_hours = new Date().getHours();
+    let get_minutes = new Date().getMinutes();
+    let get_seconds = new Date().getSeconds();
+    
+    const hours = document.querySelector('#hours')
+    hours.innerHTML = get_hours<10?"0"+get_hours:get_hours
+    const minutes =document.querySelector('#minutes')
+    minutes.innerHTML = get_minutes<10?"0"+get_minutes:get_minutes
+    const seconds = document.querySelector('#seconds')
+    seconds.innerHTML = get_seconds<10?"0"+get_seconds:get_seconds
+}
+
+setInterval(timing,1000)
